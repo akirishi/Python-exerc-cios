da49386a719017a2a8b972aaaf2bdcf8220552be
akirishi <90563712+akirishi@users.noreply.github.com>
2021-10-31 17:19:25 -0300

    Update ex010 - Conversor de moedas.py
    Retirada de valores estáticos e adição de API de consulta de cotação

--- Mundo 1/Aula 7/ex010 - Conversor de moedas.py
+++ Mundo 1/Aula 7/ex010 - Conversor de moedas.py
@@ -1,38 +1,46 @@
 # Crie um programa que leia quanto dinheiro uma pessoa
 # tem na carteira e mostre quantos dólares ela pode
 # comprar
-
 from time import sleep
+import requests  # Biblioteca para a api
 
+req = requests.get('https://economia.awesomeapi.com.br/all/')  # API
+cot = req.json()
 print('Conversão de moeda')
-brl = float(input('Qunatos reais você tem? R$: '))
-dol = brl / 5.51
-eur = brl / 6.57
-lbr = brl / 7.62
-ien = brl / 0.051
-doa = brl / 0.24
-x = int(input('Digite\n'
-              '1 para ($) Dólar\n'
-              '2 para (€) Euro\n'
-              '3 para (£) Libra Esterlina\n'
-              '4 para (¥) Iene\n'
-              '5 para (A$) Dólar Australiano\n'
-              ''))
-
+val = int(input('Digite o valor a ser convertido: R$ '))  # input do usuário
+x = int(input('1 - ($) Dólar Americano'  # menu
+              '\n2 - (€) Euro'
+              '\n3 - (£) Libra Esterlina'
+              '\n4 - (¥) Iene Japonês'
+              '\n5 - () Dólar Australiano'
+              '\n>'))
 print('=-' * 20)
 print('Calculando...')
 print('=-' * 20)
 sleep(2)
-
+# Conversão e apresentação ao usuário
 if x == 1:
-    print(f'R${brl:.2f} equivalem a ${dol:.2f}')
+    print(f'A moeda {cot["USD"]["name"]}'
+          f'\nÚltima cotação em {cot["USD"]["create_date"]}'
+          f'\nEsta no valor de R$ {cot["USD"]["bid"]}'
+          f'\nConvertendo R$ {val}, você teria U$ {val / float(cot["USD"]["bid"]):.2f}')
 elif x == 2:
-    print(f'R${brl:.2f} equivalem a €{eur:.2f}')
+    print(f'Moeda {cot["EUR"]["name"]}'
+          f'\nÚltima cotação em {cot["EUR"]["create_date"]}'
+          f'\nEstá no valor de R$ {cot["EUR"]["bid"]}'
+          f'\nConvertento R$ {val}, você teria € {val / float(cot["EUR"]["bid"]):.2f}')
 elif x == 3:
-    print(f'R${brl:.2f} equivale a £{lbr:.2f}')
+    print(f'Moeda {cot["GBP"]["name"]}'
+          f'\nÚltima cotação em {cot["GBP"]["create_date"]}'
+          f'\nEstá no valor de R$ {cot["GBP"]["bid"]}'
+          f'\nConvertento R$ {val}, você teria £ {val / float(cot["GBP"]["bid"]):.2f}')
 elif x == 4:
-    print(f'R${brl:.2f} equivale a ¥{ien:.2f}')
+    print(f'Moeda {cot["JPY"]["name"]}'
+          f'\nÚltima cotação em {cot["JPY"]["create_date"]}'
+          f'\nEstá no valor de R$ {cot["JPY"]["bid"]}'
+          f'\nConvertento R$ {val}, você teria ¥ {val / float(cot["JPY"]["bid"]):.2f}')
 elif x == 5:
-    print(f'R${brl:.2f} equivale a A${doa:.2f}')
-else:
-    print(f'Opcão inválida, tente novamente')
+    print(f'Moeda {cot["AUD"]["name"]}'
+          f'\nÚltima cotação em {cot["AUD"]["create_date"]}'
+          f'\nEstá no valor de R$ {cot["AUD"]["bid"]}'
+          f'\nConvertento R$ {val}, você teria A$ {val / float(cot["AUD"]["bid"]):.2f}')
