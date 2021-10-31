3be18ab65ecdcc3d51da0fff735fd95cfea3098e
akirishi <90563712+akirishi@users.noreply.github.com>
2021-10-31 12:07:26 -0300

    Update ex008 - Conversor de medidas.py
    Melhoria do código

--- Mundo 1/Aula 7/ex008 - Conversor de medidas.py
+++ Mundo 1/Aula 7/ex008 - Conversor de medidas.py
@@ -3,12 +3,6 @@
 
 print('Conversor de comprimento')
 m = float(input('Valor em metros: '))
-km = m / 1000
-hm = m / 100
-dam = m / 10
-dm = m * 10
-cm = m * 100
-mm = m * 1000
 x = float(input('Digite\n'
                 '1 para Km\n'
                 '2 para Hm\n'
@@ -18,14 +12,14 @@ x = float(input('Digite\n'
                 '6 para mm\n'
                 ''))
 if x == 1:
-    print(f'{m}m são {km}Km')
-if x == 2:
-    print(f'{m}m são {hm}Hm')
-if x == 3:
-    print(f'{m}m são {dam}Dam')
-if x == 4:
-    print(f'{m}m são {dm}dm')
-if x == 5:
-    print(f'{m}m são {cm}cm')
-if x == 6:
-    print(f'{m}m são {mm}mm')
+    print(f'{m}m são {m / 1000}Km')
+elif x == 2:
+    print(f'{m}m são {m / 100}Hm')
+elif x == 3:
+    print(f'{m}m são {m / 10}Dam')
+elif x == 4:
+    print(f'{m}m são {m * 10}dm')
+elif x == 5:
+    print(f'{m}m são {m * 100}cm')
+elif x == 6:
+    print(f'{m}m são {m * 1000}mm')
